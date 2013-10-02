67330eb791cf8e09a3c2487f45adacb4ee4700fa
tony <amirsgarbunk@gmail.com>
2013-10-02 15:29:41 -0700

    scaffolding for new-blog-entry page

--- app/app.rb
+++ app/app.rb
@@ -1,12 +1,22 @@
 class Blogalog < Sinatra::Base
 
+	get '/entries/new' do
+		haml :"entries/new"
+	end
+
 	get "/" do
 
 		# TODO - the query should retrieve the first entry sorted by date descending
 		entries=Entry.db_get_paged
-		initial_entry=entries && entries.count > 0 ? entries[0] : nil
+		@initial_entry=entries && entries.count > 0 ? entries[0] : nil
 
-		haml :index, :locals=>{:initial_entry=>initial_entry}
+		haml :index
 	end
 
+	helpers do
+		def logged_in?
+			#todo
+			true
+		end
+	end
 end
