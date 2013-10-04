b73bd62eef38fa3fcdc389ec74f53b3dfa122f11
tony <amirsgarbunk@gmail.com>
2013-10-04 15:19:50 -0700

    better db_insert override in Entry model

--- app/models.rb
+++ app/models.rb
@@ -1,9 +1,8 @@
 
 class Tag<Mongooz::MongoozHash; end
 class Entry<Mongooz::MongoozHash
-	alias_method :old_db_insert, :db_insert
 	def db_insert(options={})
 		self[:created_at]=Time.now
-		old_db_insert(options)
+		super(options)
 	end
 end
