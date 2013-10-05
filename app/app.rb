5ba628f74306e6d0508dad7f7a067d0b8e711bd7
tony <amirsgarbunk@gmail.com>
2013-10-05 10:55:22 -0700

    home page will now fetch latest entry for display

--- app/app.rb
+++ app/app.rb
@@ -36,11 +36,21 @@ class Blogalog < Sinatra::Base
 
 	get "/" do
 
-		# TODO - the query should retrieve the first entry sorted by date descending
-		entries=Entry.db_get_paged
-		@initial_entry=entries && entries.count > 0 ? entries[0] : nil
+		# mongooz is too dumb to do all the sorting and junk ... gotta do it myself. fail.
+		mongooz_opts=Entry.set_db_options Mongooz.defaults
+		result=nil
+		begin
+			Mongooz::Base.collection(mongooz_opts) do |col|
+				entries=col.find().sort({:created_at=>-1}).limit(1)
+				raise "Got no entries from sorted query" unless entries
+				raise "Expected 1 result and got #{entries.count} instead" unless entries.count(true)==1
+				result=Entry.typified_result_hash_or_nil entries.next
+			end
+		rescue Exception=>e
+			result=Entry.typified_result_hash_or_nil({:title=>'There was an error retrieving the first blog entry!?',:body=>e.message})
+		end
 
-		haml :index
+		haml :index, :locals=>{:initial_entry=>result}
 	end
 
 	helpers do
